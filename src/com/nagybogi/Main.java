63752d37d4538dcfdf7a5149b8a7b2f3b8d316ac
nagybogie <nagy.bogie@gmail.com>
2017-11-05 18:01:07 +0100

    Commenting out array

--- src/com/nagybogi/Main.java
+++ src/com/nagybogi/Main.java
@@ -7,23 +7,23 @@ public class Main {
         int n = 4;
         int m = 2*n + 2;
 
-        String[][] arr = new String[m][m];
+        //String[][] arr = new String[m][m];
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < m; j++) {
                 if((i == 0) || (i == m-1)) {
-                    arr[i][j] = "*";
+                    //arr[i][j] = "*";
                     System.out.print("*");
                 } else if((j == 0) || (j == m-1)) {
-                    arr[i][j] = "*";
+                    //arr[i][j] = "*";
                     System.out.print("*");
                 } else if((i == j) && (i != 0) && (i != m)) {
-                    arr[i][j] = "\\";
+                    //arr[i][j] = "\\";
                     System.out.print("\\");
                 } else if((i == m-1-j) && (j != 0) && (j != m)) {
-                    arr[i][j] = "/";
+                    //arr[i][j] = "/";
                     System.out.print("/");
                 } else {
-                    arr[i][j] = "*";
+                    //arr[i][j] = "*";
                     System.out.print("*");
                 }
                 if(j == m-1) {
