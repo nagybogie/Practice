4e04e0386d7bc9f0b0f8ce98f59ae8b394e77134
nagybogie <nagy.bogie@gmail.com>
2017-11-05 17:46:28 +0100

    Frist commit

---
+++ src/com/nagybogi/Main.java
@@ -0,0 +1,36 @@
+package com.nagybogi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int n = 4;
+        int m = 2*n + 2;
+
+        String[][] arr = new String[m][m];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < m; j++) {
+                if((i == 0) || (i == m-1)) {
+                    arr[i][j] = "*";
+                    System.out.print("*");
+                } else if((j == 0) || (j == m-1)) {
+                    arr[i][j] = "*";
+                    System.out.print("*");
+                } else if((i == j) && (i != 0) && (i != m)) {
+                    arr[i][j] = "\\";
+                    System.out.print("\\");
+                } else if((i == m-1-j) && (j != 0) && (j != m)) {
+                    arr[i][j] = "/";
+                    System.out.print("/");
+                } else {
+                    arr[i][j] = "*";
+                    System.out.print("*");
+                }
+                if(j == m-1) {
+                    System.out.println();
+                }
+            }
+        }
+
+    }
+}
